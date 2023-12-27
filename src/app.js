7cdd82f0571dfe9829373a5c49667eb9a585a403
Ágatha <agathamagnusg@hotmail.com>
2023-12-27 15:25:29 -0300

    creating a route for books

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello World');
-})
+const books = [
+  {
+    id: 1,
+    title: "Harry Potter and the Philosopher's Stone",
+  },
+  {
+    id: 2,
+    title: "Harry Potter and the Chamber of Secrets",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World");
+});
+
+app.get("/books", (req, res) => {
+  res.status(200).json(books);
+});
 
 export default app;
